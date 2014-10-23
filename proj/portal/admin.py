432f5e73d4361a9f26540c7e107f585772cf67fb
Vincent <vandrada1248@gmail.com>
2014-10-22 20:15:53 -0500

    added User, Game, and Review

--- proj/portal/admin.py
+++ proj/portal/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from portal.models import User, Game, Review
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Game)
+admin.site.register(Review)
