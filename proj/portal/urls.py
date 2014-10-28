a94dec8685ff1e178779e5fd413a88322e43f0f6
Vincent <vandrada1248@gmail.com>
2014-10-27 21:56:18 -0500

    added proper game prefix; added vote prefix

--- proj/portal/urls.py
+++ proj/portal/urls.py
@@ -3,6 +3,9 @@ from portal import views
 
 
 urlpatterns = patterns('',
-                       url(r'^(?P<game_id>\d+)/$', views.game, name='game'),
-                       url(r'^home$', views.home, name='home')
+                       url(r'^home$', views.home, name='home'),
+                       url(r'^game/(?P<game_id>\d+)/$',
+                           views.game, name='game'),
+                       url(r'^vote/(?P<question_id>\d+)/$',
+                           views.vote, name='vote'),
                        )
