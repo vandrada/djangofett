011ab934fcd608d6bc40c3042f848375fb8e62aa
Vincent <vandrada1248@gmail.com>
2014-10-29 17:21:30 -0500

    added review URL (and screw flake8)

--- proj/portal/urls.py
+++ proj/portal/urls.py
@@ -3,9 +3,8 @@ from portal import views
 
 
 urlpatterns = patterns('',
-                       url(r'^home/$', views.home, name='home'),
-                       url(r'^game/(?P<game_id>\d+)/$',
-                           views.game, name='game'),
-                       url(r'^vote/(?P<question_id>\d+)/$',
-                           views.vote, name='vote'),
-                       )
+    url(r'^home/$', views.home, name='home'),
+    url(r'^game/(?P<game_id>\d+)/$', views.game, name='game'),
+    url(r'^vote/(?P<question_id>\d+)/$', views.vote, name='vote'),
+    url(r'review/(?P<review_id>\d+)/$', views.review, name='review'),
+)
