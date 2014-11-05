7c3ce487f3d22ed4711ddb212d67f675c533f3e8
Vincent <vandrada1248@gmail.com>
2014-11-04 20:55:10 -0600

    anchor review URL with leading ^

--- proj/portal/urls.py
+++ proj/portal/urls.py
@@ -6,10 +6,8 @@ urlpatterns = patterns('',
     url(r'^home/$', views.home, name='home'),
     url(r'^game/(?P<game_id>\d+)/$', views.game, name='game'),
     url(r'^vote/(?P<question_id>\d+)/$', views.vote, name='vote'),
-    url(r'review/(?P<review_id>\d+)/$', views.review, name='review'),
-    
+    url(r'^review/(?P<review_id>\d+)/$', views.review, name='review'),
     url(r'^platform/(?P<platform_id>\d+)/$', views.game_list, name='game_list'),
-    
     # For User Control
     url(r'^doreg/', views.userctrl_doreg, name='usr_reg'),
     url(r'^login/', views.userctrl_login, name='usr_login'),
